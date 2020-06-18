c09bf17cc2f04e570555604bbcc57a9f7b7064e6
mbalc <mbalc909@gmail.com>
2020-06-18 18:12:07 +0200

    Add custom class for coordinates to ensure these are always in board range

---
+++ src/consts.hpp
@@ -0,0 +1,8 @@
+#ifndef TIC_TAC_TOE_CONSTS_HPP
+#define TIC_TAC_TOE_CONSTS_HPP
+
+namespace TicTacToe {
+    const int BOARD_WIDTH = 3;
+}
+
+#endif //TIC_TAC_TOE_CONSTS_HPP
--- src/coords.hpp
+++ src/coords.hpp
@@ -1,8 +1,32 @@
 #ifndef TIC_TAC_TOE_COORDS_HPP
 #define TIC_TAC_TOE_COORDS_HPP
 
+#include "consts.hpp"
+
 namespace TicTacToe {
-    using Coordinate = int;
+    class Coordinate {
+        int value;
+
+    public:
+        Coordinate(int initValue = 0) : value(initValue) {}
+
+        Coordinate *operator=(int c) {
+            value = c;
+            return this;
+        }
+
+        Coordinate operator+(int c) const { return Coordinate(value + c); }
+
+        Coordinate *operator+=(int c) { return this->operator=(value + c); }
+
+        Coordinate *operator-=(int c) { return *this += (-c); }
+
+        operator int() {
+            value %= BOARD_WIDTH;
+            return value;
+        }
+    };
+
     using Coords = Coordinate[3];
 }
 
