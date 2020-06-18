e4bce0fa416348320f828c2918844efc761d0190
mbalc <mbalc909@gmail.com>
2020-06-18 17:43:45 +0200

    Fix problems with compiling game.hpp

--- src/board.hpp
+++ src/board.hpp
@@ -10,7 +10,7 @@ namespace TicTacToe {
         std::optional <Symbol> fieldContent(Coords coords);
 
         std::vector<Coords[3]> winningSymbolLines();
-    }
-};
+    };
+}
 
 #endif //TIC_TAC_TOE_BOARD_H
--- src/coords.hpp
+++ src/coords.hpp
@@ -2,8 +2,8 @@
 #define TIC_TAC_TOE_COORDS_HPP
 
 namespace TicTacToe {
-    typename Coordinate int;
-    typename Coords Coordinate[3];
+    using Coordinate = int;
+    using Coords = Coordinate[3];
 }
 
 #endif //TIC_TAC_TOE_COORDS_HPP
